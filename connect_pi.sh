197c5acdc6dcc35a904ad43452665c851272edf3
yotamshwix <yotamsh@wix.com>
2025-10-31 00:24:58 +0200

    working wierd connect with venv

--- connect_pi.sh
+++ connect_pi.sh
@@ -4,10 +4,13 @@
 #
 
 echo "Connecting to Raspberry Pi..."
-ssh -t yotam@raspberrypi.local "cd /home/yotam/AmpSync/Amp3 && source venv_pi/bin/activate && exec bash"
+ssh -t yotam@raspberrypi.local "cd /home/yotam/AmpSync/Amp3; source venv_pi/bin/activate; bash --rcfile <(echo 'PS1=\"(venv_pi) \u@\h:\w\$ \"')"
 
 # To run the main.py file:
 # sudo ./venv_pi/bin/python3 main.py
 
 # To shutdown the Raspberry Pi:
 # sudo shutdown -h now
+
+# To disconnect from the ssh (CTRL + D)
+# exit
